425c069f03a7481cfc35fa0616f88d6567063758
Vikaskanwate <vikaskanwate32@gmail.com>
2026-03-06 12:56:11 +0530

    practice

---
+++ ResumeBackend/src/test/java/com/phoenix/ResumeBackend/ResumeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenix.ResumeBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
